79a6f1a4aa93894a9c5d1bfb9b8e1500a08f84d6
mseiyai <mseiyai@gmail.com>
2020-11-25 12:46:24 +0300

    Gridsome – Explore plugins to make life easy

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,20 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'PrimaryScTz',
+  siteDescription: "Tanzania Primary Schools",
+  siteUrl: 'https://primary.sc.tz',
+  titleTemplate: `%s | Tanzania Primary Schools`,
+  icon: 'src/favicon.png',
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss"
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },  
+  ]
 }
